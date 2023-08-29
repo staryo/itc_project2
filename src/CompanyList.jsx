8102da447afa219095737a3a3049b526fa50caaa
Артем Столов <artem.stolov@gmail.com>
2023-08-29 14:01:41 +0300

    error handling in CompanyList.jsx

--- src/CompanyList.jsx
+++ src/CompanyList.jsx
@@ -32,13 +32,29 @@ function CompanyList(companyList) {
                         <tbody id="table-content" className="bg-transparent">
                         {
                             optionsList.map(option => (
-                                <tr key={option.symbol} className="bg-transparent p-5 border-bottom link-row" onClick={() => {
-                                    handleSubmitfunc(`company/${option.symbol}?search=${queryParameters.get("search")}`)
+                                <tr key={option.symbol} className={
+                                    `bg-transparent p-5 border-bottom ${
+                                        option.symbol !== 'ERROR' ? "link-row" : ""
+                                    }`
+                                } onClick={() => {
+                                    if (option.symbol !== 'ERROR') {
+                                        handleSubmitfunc(
+                                            `company/${option.symbol}?search=${queryParameters.get("search")}`
+                                        )
+                                    }
                                 }}>
-                                    <th scope="row" className="col-1 text-center px-3 py-2">{option.symbol}</th>
-                                    <td className="col-5 text-center px-3 py-2">{option.name}</td>
-                                    <td className="col-2 text-center px-3 py-2 d-none d-lg-table-cell">{option.currency}</td>
-                                    <td className="col-4 text-center d-none d-lg-table-cell px-3 py-2">{option.stockExchange}</td>
+                                    <th scope="row" className="col-1 text-center px-3 py-2">
+                                        {option.symbol}
+                                    </th>
+                                    <td className="col-5 text-center px-3 py-2">
+                                        {option.name}
+                                    </td>
+                                    <td className="col-2 text-center px-3 py-2 d-none d-lg-table-cell">
+                                        {option.currency}
+                                    </td>
+                                    <td className="col-4 text-center d-none d-lg-table-cell px-3 py-2">
+                                        {option.stockExchange}
+                                    </td>
                                 </tr>
                             ))
                         }
