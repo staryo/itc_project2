d314653fdf9effee9ec641dc1ff07a9adbd1c93f
Артем Столов <artem.stolov@gmail.com>
2023-09-06 17:06:33 +0300

    [M11] page should present the companies chosen side by side

--- src/ui/homePage/CompanyListRows.jsx
+++ src/ui/homePage/CompanyListRows.jsx
@@ -91,6 +91,8 @@ function CompanyListRows(props) {
                         <td className="col-2 text-center d-none d-lg-table-cell px-3 py-2">
                             <button className={`btn btn-light ${
                                 props.compareList.length >= 4 ? "d-none" : ""
+                            } ${
+                                option.symbol === 'ERROR' ? "d-none" : ""
                             }`} onClick={
                                 () => {
                                     props.setCompareList(option.symbol)
