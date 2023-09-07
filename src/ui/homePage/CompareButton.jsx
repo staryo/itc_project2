c84a53a92331a0200994ba74cc3ff82ba711541e
Артем Столов <artem.stolov@gmail.com>
2023-09-07 15:33:54 +0300

    [M11] CompareButton.jsx correction for "WAIT" status

--- src/ui/homePage/CompareButton.jsx
+++ src/ui/homePage/CompareButton.jsx
@@ -14,7 +14,7 @@ export default function CompareButton(props) {
                 <button className={`btn btn-light ${
                     props.currentNumber >= 4 ? "d-none" : ""
                 } ${
-                    props.symbol in ['ERROR', 'WAIT'] ? "d-none" : ""
+                    ['ERROR', 'WAIT'].includes(props.symbol) ? "d-none" : ""
                 }`} onClick={
                     () => {
                         props.setCompareList(props.symbol)
